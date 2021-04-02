2f625137bcf2115f440c3dc6d75c8b93cb9efa30
unknown <xevierturrubiartes@gmail.com>
2021-04-02 11:02:03 -0500

    yup

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
     PORT: process.env.PORT || 8000,
     API_ENDPOINT: 
-        'https://neighbr-api.herokuapp.com',
+        'http://localhost:8000',
     TOKEN_KEY: 'neighbr-auth-token'
 }
 // https://neighbr-api.herokuapp.com
